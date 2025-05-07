284d0cf30a123af060e9c6e14f113396ef345a5e
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-05-07 17:40:36 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,30 @@
 const express=require("express");
 require("dotenv").config();
 const app=express();
- const cors=require("cors");
+const cors=require("cors");
+const { connectdb } = require("./db/db");
+const { router }=require('./Routes/route')
 const PORT=process.env.PORT ;
+const URL=process.env.MONGO_URL
+
+
+app.use(express.json());
 
 app.use(cors({
     methods:['GET','POST','PATCH','DELETE'],
     origin:"http://localhost:5173"
 }))
 
-app.use(express.json());
+
+app.use("/api",router);
 
 
-app.get("/",(req,res)=>{
-    res.json("TASK_TRACKER")
-})
 
-app.listen(PORT,()=>{
-    console.log(`server is listening to ${PORT}`);
-})
+const start=async ()=>{
+    await connectdb(URL);
+    app.listen(PORT,()=>{
+        console.log(`server is listening to ${PORT}`);
+    })
+}
+
+start();
