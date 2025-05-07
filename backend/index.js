3c54d6d5ee7af300c073e02cf3c4d3e7824f07ac
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-05-07 12:33:59 +0530

    Enabled CORS

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log("TASK_TRACKER")
+const express=require("express");
+require("dotenv").config();
+const app=express();
+ const cors=require("cors");
+const PORT=process.env.PORT ;
+
+app.use(cors({
+    methods:['GET','POST','PATCH','DELETE'],
+    origin:"http://localhost:5173"
+}))
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.json("TASK_TRACKER")
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is listening to ${PORT}`);
+})
