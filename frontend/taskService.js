67b8d4efa3952c673700a354ffeeea1f282603ac
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-05-08 22:12:30 +0530

    Implemented addingTasks

--- frontend/taskService.js
+++ frontend/taskService.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const displaytasks = async () => {
   try {
     const response = await fetch("http://localhost:3000/api/");
@@ -6,5 +8,19 @@ export const displaytasks = async () => {
     return data.tasks;
   } catch (e) {
     console.log(e);
+  }
+};
+
+export const addingTasks = async (title, description) => {
+  try {
+    console.log(title);
+    console.log(description);
+    const response = await axios.post("http://localhost:3000/api/create", {
+      name: title,
+      description: description,
+    });
+    return response.data;
+  } catch (e) {
+    return e;
   }
 };
